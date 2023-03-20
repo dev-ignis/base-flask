26e77a529314e75634fdf583ce39a7fed6677cbe
dev-ignis <maildevcom+1@gmail.com>
2023-03-20 19:26:13 +0000

    feat: - base structure;

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, abort, render_template
+import datetime
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+
+@app.route('/add/<int:n1>/<int:n2>/')
+def add(n1, n2):
+    return '<h1>{}</h1>'.format(n1 + n2)
+
+
+@app.route('/users/<int:user_id>/')
+def greet_user(user_id):
+    users = ['Bob', 'Jane', 'Adam']
+    try:
+        return '<h2>Hi {}</h2>'.format(users[user_id])
+    except IndexError:
+        abort(404)
+
+@app.route('/comments/')
+def comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
+
+    return render_template('comments.html', comments=comments)
+
+
+@app.route('/messages/<int:idx>')
+def message(idx):
+    app.logger.info('Building the messages list...')
+    messages = ['Message Zero', 'Message One', 'Message Two']
+    try:
+        app.logger.debug('Get message with index: {}'.format(idx))
+        return render_template('message.html', message=messages[idx])
+    except IndexError:
+        app.logger.error('Index {} is causing an IndexError'.format(idx))
+        abort(404)
+
+
+# TODO: should be removed;
+@app.route('/500')
+def error500():
+    abort(500)
